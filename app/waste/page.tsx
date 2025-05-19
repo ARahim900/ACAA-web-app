2b73959026396814bf1ce79d321c000aa3cf3052
Abdularahim Al Balushi <143422547+ARahim900@users.noreply.github.com>
2025-05-19 08:30:24 +0400

    Add Waste Management route page

---
+++ app/waste/page.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+import WasteManagementDashboard from "@/features/waste-management/components/waste-management-dashboard"
+
+export default function WastePage() {
+  return <WasteManagementDashboard />
+}
